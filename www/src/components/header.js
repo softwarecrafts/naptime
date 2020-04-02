6ff9e3fb212b0ff05b191e66225ac34ce51b7a51
Andrew Miller <info@akmiller.co.uk>
2020-04-02 21:36:40 +0100

    make the darkmode button nicer and still accessible

--- www/src/components/header.js
+++ www/src/components/header.js
@@ -8,14 +8,14 @@ const Header = () => {
   return (
     <>
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 w-screen h-full">
-        <nav className={`relative flex items-center justify-between sm:h-10 md:justify-center `}>
+        <nav className={`relative flex items-center justify-between sm:h-10 md:justify-center`}>
           <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
             <div className="flex items-center justify-between w-full md:w-full">
-              <div className="flex flex-row">
+              <div className="flex flex-row justify-center">
                 {theme.name === "dark" ? (
                   <button
                     type="button"
-                    className="ml-4 py-2 inline-block"
+                    className={`ml-4 mr-1 py-1 rounded-full inline-block self-center focus:outline-none ${theme.text.accent7}  hover:${theme.bg.accent1} hover:${theme.text.inverse} focus:${theme.bg.accent1} focus:${theme.text.inverse}`}
                     onClick={() =>
                       dispatch({
                         type: "changeTheme",
@@ -30,7 +30,7 @@ const Header = () => {
                     strokeLinejoin="round"
                     strokeWidth="2"
                     viewBox="0 0 24 24"
-                    className={`${theme.text.accent7} inline w-6 h-6 mx-1 hover:${theme.text.accent3}`}
+                    className={`inline w-6 h-6 mx-1`}
                   >
                     <path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path>
                   </svg>
@@ -38,7 +38,7 @@ const Header = () => {
                 ) : (
                   <button
                     type="button"
-                    className="ml-4 py-2 inline-block"
+                    className={`ml-4 mr-1 py-1 rounded-full inline-block self-center focus:outline-none ${theme.text.accent7}  hover:${theme.bg.accent1} hover:${theme.text.inverse} focus:${theme.bg.accent1} focus:${theme.text.inverse}`}
                     onClick={() =>
                       dispatch({
                         type: "changeTheme",
@@ -53,7 +53,7 @@ const Header = () => {
                     strokeLinejoin="round"
                     strokeWidth="2"
                     viewBox="0 0 24 24"
-                    className={`${theme.text.accent7} inline w-6 h-6 mx-1 hover:${theme.text.accent3}`}
+                    className={`inline w-6 h-6 mx-1`}
                   >
                     <path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path>
                   </svg>
