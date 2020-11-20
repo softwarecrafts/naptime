598fa23cbd1b993595923e1022643f1823f79919
Andrew Miller <info@akmiller.co.uk>
2020-11-20 17:55:42 +0000

    prettier SEO component

--- www/src/components/seo.js
+++ www/src/components/seo.js
@@ -5,10 +5,10 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 import { useStateValue } from "../utils/state";
 
 function SEO({ description, lang, meta, title }) {
@@ -24,16 +24,16 @@ function SEO({ description, lang, meta, title }) {
         }
       }
     `
-  )
+  );
   const [{ theme }] = useStateValue();
 
-  const metaDescription = description || site.siteMetadata.description
+  const metaDescription = description || site.siteMetadata.description;
 
   return (
     <Helmet
       htmlAttributes={{
         lang,
-        class: theme.bg.normal
+        class: theme.bg.normal,
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
@@ -70,32 +70,61 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
-      ].concat(meta)}
-    >
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,200,300,400,500,600,700,800,900|Roboto:100,300,400,500,700,900&display=swap" rel="stylesheet"/>
-        <link rel="apple-touch-icon" sizes="180x180" href={`/icons/${theme.name}/apple-touch-icon.png`}/>
-        <link rel="icon" type="image/png" sizes="32x32" href={`/icons/${theme.name}/favicon-32x32.png`}/>
-        <link rel="icon" type="image/png" sizes="16x16" href={`/icons/${theme.name}/favicon-16x16.png`}/>
-        <link rel="manifest" href={`/icons/${theme.name}/site.webmanifest`}/>
-        <link rel="mask-icon" href={`/icons/${theme.name}/safari-pinned-tab.svg`} color="#a4baed"/>
-        <meta name="msapplication-TileColor" content="#a4baed"/>
-        <meta name="theme-color" content="#a4baed"/>
-        <script data-name="BMC-Widget" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="softwarecrafts" data-description="Support naptime with Green Tea!" data-message="Thank you for your donation. Every tea helps!" data-color="#5F7FFF" data-position="left" data-x_margin="18" data-y_margin="18"></script>
-    </Helmet>u
-  )
+      ].concat(meta)}>
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,200,300,400,500,600,700,800,900|Roboto:100,300,400,500,700,900&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={`/icons/${theme.name}/apple-touch-icon.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href={`/icons/${theme.name}/favicon-32x32.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={`/icons/${theme.name}/favicon-16x16.png`}
+      />
+      <link rel="manifest" href={`/icons/${theme.name}/site.webmanifest`} />
+      <link
+        rel="mask-icon"
+        href={`/icons/${theme.name}/safari-pinned-tab.svg`}
+        color="#a4baed"
+      />
+      <meta name="msapplication-TileColor" content="#a4baed" />
+      <meta name="theme-color" content="#a4baed" />
+      <script
+        data-name="BMC-Widget"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="softwarecrafts"
+        data-description="Support naptime with Green Tea!"
+        data-message="Thank you for your donation. Every tea helps!"
+        data-color="#5F7FFF"
+        data-position="left"
+        data-x_margin="18"
+        data-y_margin="18"></script>
+    </Helmet>
+  );
 }
 
 SEO.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
-}
+};
 
 SEO.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
-}
+};
 
-export default SEO
+export default SEO;
